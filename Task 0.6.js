f4e9918f23093f2a28751c4ad67317765b7bccdf
Sthembiso Radebe <sthembisor375@gmail.com>
2021-02-16 22:37:34 +0200

    task 0.7 removed first function

--- Task 0.6.js
+++ Task 0.6.js
@@ -1,19 +1,3 @@
-function maximumNumber(a, b, c) {
-
-    var myArray = [a, b, c];
-    var maxNumber = myArray[0];
-
-    for (var i = 0; i < myArray.length; i++) {
-        if (myArray[i] >= maxNumber) {
-            maxNumber = myArray[i];
-        }
-    }
-
-    console.log(maxNumber);
-}
-
-maximumNumber(2, 4, 6);
-
 function maximumNumberOpenList(...openList) {
 
     var myArrayOpen = [...openList];
